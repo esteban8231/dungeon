617ec28d2155a5ff7c8a4c11ff8b9cde5f52eac3
Quete <yo8231@gmail.com>
2019-11-14 21:20:55 -0500

    Character views controller

--- app/controllers/character_views_controller.rb
+++ app/controllers/character_views_controller.rb
@@ -1,5 +1,10 @@
 class CharacterViewsController < ApplicationController
   def create
-    
+    #Este es el que cambia el cookie y hace toda la vaina.
+    cookies[:character_id] = params[:character_id]
+    @character = Character.find(params[:character_id])
+    respond_to do |format|
+      format.js {render 'create'}
+    end
   end
 end
