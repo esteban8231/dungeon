13bfda260ed12aa227741e274c626f6a21b8c6ab
Quete <yo8231@gmail.com>
2019-11-14 20:48:54 -0500

    Finished attacks controller

--- app/controllers/attacks_controller.rb
+++ app/controllers/attacks_controller.rb
@@ -1,9 +1,24 @@
 class AttacksController < ApplicationController
   def create
-
+    @attack = Attack.new(attack_params)
+    @character = @attack.character
+    if @attack.save
+      redirect_to @character
+    else
+      render @character
+    end
   end
 
   def delete
-    
+    @attack = Attack.find(params[:id])
+    @character = @attack.character
+    @attack.destroy!
+    redirect_to @character
+  end
+
+  private
+
+  def attack_params
+    params.require(:attack).permit(:attack_bonus, :damage_dice, :character_id, :weapon)
   end
 end
