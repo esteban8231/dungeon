363333a1caef1be8047c80206bd7104c1290f8d7
Quete <yo8231@gmail.com>
2019-11-14 22:00:33 -0500

    Finished all controllers

--- app/controllers/tiles_controller.rb
+++ app/controllers/tiles_controller.rb
@@ -1,5 +1,11 @@
 class TilesController < ApplicationController
   def update
-    
+    puts params
+  end
+
+  private
+
+  def tile_params
+    params.require(:tile).permit(:tile_type, :icon)
   end
 end
